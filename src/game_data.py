9be147637ae450781c1ff08d7955917f79b03500
Murat65536 <bmuratb502@gmail.com>
2024-08-10 23:08:33 -0400

    Change items to scale dependent to size.

--- src/game_data.py
+++ src/game_data.py
@@ -722,6 +722,7 @@ def parse_item_data():
 		item_data["hold_offset"] = float(item_data["hold_offset"])
 		try:
 			loaded_surf = pygame.image.load(item_data["image_path"]).convert_alpha()
+			loaded_surf = pygame.transform.scale(loaded_surf, (loaded_surf.get_width() * (32 / max(loaded_surf.get_width(), loaded_surf.get_height())), loaded_surf.get_height() * (32 / max(loaded_surf.get_width(), loaded_surf.get_height()))))
 			item_data["image"] = loaded_surf
 			item_data["item_slot_offset_x"] = int(24 - item_data["image"].get_width() * 0.5)
 			item_data["item_slot_offset_y"] = int(24 - item_data["image"].get_height() * 0.5)
@@ -737,7 +738,7 @@ def parse_item_data():
 			item_data["crit_chance"] = float(item_data["crit_chance"])
 			try:
 				loaded_surf = pygame.image.load(item_data["world_override_image_path"]).convert_alpha()
-				item_data["world_override_image"] = pygame.Surface((loaded_surf.get_width(), loaded_surf.get_height()))
+				item_data["world_override_image"] = pygame.Surface((max(loaded_surf.get_width(), loaded_surf.get_height()), max(loaded_surf.get_width(), loaded_surf.get_height())))
 			except FileNotFoundError:
 				item_data["world_override_image"] = None
 			except pygame.error:
